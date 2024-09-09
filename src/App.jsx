6c0e2251716039d68ec328e87e00fc1745312569
hakhan1999 <khanhassaan961@gmail.com>
2024-09-09 13:32:34 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "./index";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
